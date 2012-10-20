8e005455e44ffbacd35b0789c9c883e02aaf352a
Albert-Jan Nijburg <albertjan@curit.com>
2012-10-20 13:55:37 +0200

    Some comments

--- Domotics.Base.Test/RuleLogicFixture.cs
+++ Domotics.Base.Test/RuleLogicFixture.cs
@@ -22,18 +22,28 @@ namespace Domotics.Base.Test
         [Test]
         public void SimpleRuleCheckTest ()
         {
+            //given
             var rule = new Rule(@"When(""knopje"").IsPushed().Turn(""lampje"")(""on"")", new[] {"knopje", "lampje"});
+
+            //when
             Distributor.RuleStores.First().AddRule (rule);
+
+            //then
             Assert.IsTrue(rule.Check());
         }
 
         [Test]
         public void SimpleRuleFireTest ()
         {
+            //given
             var rule = new Rule (@"When(""knopje"").IsPushed().Turn(""lampje"")(""on"")", new[] { "knopje", "lampje" });
             Distributor.RuleStores.First ().AddRule (rule);
             Distributor.ExternalSources.First().Connections.First(c => c.Name == "knopje").CurrentState = "in";
+
+            //when
             var chd = rule.Fire(new Connection("knopje", ConnectionType.In) {CurrentState = "in"}, "out");
+            
+            //then
             Assert.AreEqual("lampje",chd.Connection.Name);
             Assert.AreEqual("on", chd.NewState.Name);
         }
@@ -41,19 +51,29 @@ namespace Domotics.Base.Test
         [Test]
         public void InputEventRuleFireTest ()
         {
+            //given
             var rule = new Rule (@"When(""knopje"").IsPushed().Turn(""lampje"")(""on"")", new[] { "knopje", "lampje" });
             Distributor.RuleStores.First ().AddRule (rule);
+            
+            //when
             ((FakeExternalSource) Distributor.ExternalSources.First ()).FireInputEvent ("knopje", "in");
             ((FakeExternalSource) Distributor.ExternalSources.First ()).FireInputEvent ("knopje", "out");
+            
+            //then
             Assert.AreEqual("on", Distributor.ExternalSources.First().Connections.First(c => c.Name == "lampje").CurrentState.Name);
         }
 
         [Test]
         public void RuleChangesStateTest ()
         {
+            //given
             var rule = new Rule (@"When(""knopje"").ChangesState(""out"",""in"").Turn(""lampje"")(""on"")", new[] { "knopje", "lampje" });
             Distributor.RuleStores.First ().AddRule (rule);
+            
+            //when
             ((FakeExternalSource)Distributor.ExternalSources.First ()).FireInputEvent ("knopje", "in");
+            
+            //then
             Assert.AreEqual ("on", Distributor.ExternalSources.First ().Connections.First (c => c.Name == "lampje").CurrentState.Name);
         }
     }
