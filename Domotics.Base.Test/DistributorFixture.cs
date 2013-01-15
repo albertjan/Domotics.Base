514b038fd7ef115721bfddb4a5f813fa4dba00ed
Albert-Jan Nijburg <albertjan@curit.com>
2013-01-15 22:31:32 +0100

    And here are the tests. And some more stuff I
    forgot to save

--- Domotics.Base.Test/DistributorFixture.cs
+++ Domotics.Base.Test/DistributorFixture.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using Castle.Components.DictionaryAdapter;
 using Domotics.Base.Test.Fakes;
 using NUnit.Framework;
 
@@ -12,7 +11,7 @@ namespace Domotics.Base.Test
         public void Init()
         {
             FakeExternalSource = new FakeExternalSource();
-            Distributor = new Distributor(new[] { FakeExternalSource }, new[] {new FakeRuleStore()});
+            Distributor = new Distributor(new[] { FakeExternalSource }, new[] { new FakeRuleStore() });
         }
 
         private FakeExternalSource FakeExternalSource { get; set; }
@@ -30,6 +29,34 @@ namespace Domotics.Base.Test
         
             //then
             Assert.That ("in" == FakeExternalSource.Connections.First (c => c.Name == "knopje").CurrentState.Name, Is.True.After (100));
+        }
+
+        [Test]
+        public void InputEventHandlerTestTwee()
+        {
+            //given
+            Assert.AreEqual("out", FakeExternalSource.Connections.First(c => c.Name == "knopje").CurrentState.Name);
+
+            //when
+            FakeExternalSource.FireInputEvent("knopje", "in");
+            FakeExternalSource.FireInputEvent("knopje", "out");
+
+            //then
+            Assert.That("out" == FakeExternalSource.Connections.First(c => c.Name == "knopje").CurrentState.Name, Is.True.After(100));
+        }
+
+        [Test]
+        public void AddRuleTest()
+        {
+            //given
+            var rulestore = Distributor.RuleStores.First();
+
+            //when
+            rulestore.AddRule(new Rule(@"When(""knopje"").IsPushed().Switch(""lampje"")", "knopje", "lampje"));
+            
+            //then
+            Assert.IsTrue(Distributor.RuleStores.First().Rules.Count() == 1);
+            Assert.IsTrue(Distributor.RuleStores.First().Rules.First().Connections.Count() == 2);
         }
     }
 }
